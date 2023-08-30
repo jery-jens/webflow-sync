ba6b922843a84d6fd408a6a8b54cc0095e37e873
devaddmore <97600411+jery-jens@users.noreply.github.com>
2023-08-30 11:34:45 +0200

    update workflow

--- transform.js
+++ transform.js
@@ -2,4 +2,6 @@ import { readFileSync, writeFileSync } from 'fs';
 
 const jsonContent = readFileSync('test.json', 'utf8');
 
-writeFileSync('transformed.json', JSON.stringify({"data": "data"}), 'utf8');
+const data = "test"
+
+writeFileSync('test.css', data, 'utf8');
