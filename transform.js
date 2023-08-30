4f838d4972e153bd9e785a5c48a4cce4f41b1e1f
devaddmore <97600411+jery-jens@users.noreply.github.com>
2023-08-30 11:58:16 +0200

    update workflow

--- transform.js
+++ transform.js
@@ -5,7 +5,7 @@ const data = JSON.parse(jsonContent);
 
 const css = Object.keys(data.Primitives).map(key => {
     const item = data.Primitives[key];
-    return `--${key}: ${item.value}`;
+    return `--${key}: ${item.value};`;
 }).join('\n');
 
 writeFileSync('test.css', css, 'utf8');
