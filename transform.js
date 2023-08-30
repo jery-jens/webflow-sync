d348e05f44671eab7b08f70293e10950e15f79ea
devaddmore <97600411+jery-jens@users.noreply.github.com>
2023-08-30 11:55:37 +0200

    update workflow

--- transform.js
+++ transform.js
@@ -1,7 +1,11 @@
 import { readFileSync, writeFileSync } from 'fs';
 
 const jsonContent = readFileSync('test.json', 'utf8');
+const data = JSON.parse(jsonContent);
 
-const data = "test"
+const css = Object.keys(data.Primitives).map(key => {
+    const item = data.Primitives[key];
+    return `--${key}: ${item.value}`;
+}).join('\n');
 
-writeFileSync('test.css', data, 'utf8');
+writeFileSync('test.css', css, 'utf8');
